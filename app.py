32a70503a0e44194527e83461be147b5c1f94cfe
Diana Lam <Diana.Lam06@gmail.com>
2018-06-17 13:43:23 -0700

    adding data showing a simple graph

--- app.py
+++ app.py
@@ -1,14 +1,74 @@
-from flask import Flask, render_template, request, redirect
+from __future__ import print_function
+import flask
+#from flask import Flask, render_template, request, redirect
 
-app = Flask(__name__)
+
+#import flask
+
+from bokeh.embed import components
+from bokeh.plotting import figure
+from bokeh.resources import INLINE
+from bokeh.util.string import encode_utf8
+import pandas as pd
+
+app = flask.Flask(__name__)
+
+colors = {
+    'Black': '#000000',
+    'Red':   '#FF0000',
+    'Green': '#00FF00',
+    'Blue':  '#0000FF',
+}
+
+def getitem(obj, item, default):
+    if item not in obj:
+        return default
+    else:
+        return obj[item]
+
+#@app.route('/')
+#def index():
+#  return render_template('index.html')
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+    """ Very simple embedding of a polynomial chart
+    """
 
-@app.route('/about')
-def about():
-  return render_template('about.html')
+    # Grab the inputs arguments from the URL
+    args = flask.request.args
+
+    # Get all the form arguments in the url with defaults
+    color = getitem(args, 'color', 'Black')
+    #_from = int(getitem(args, '_from', 0))
+    #to = int(getitem(args, 'to', 10))
+
+    # Create a polynomial line graph with those arguments
+    aapl = pd.read_csv('./data/WIKI-AAPL.csv',  parse_dates = ["Date"])
+
+    
+    #x = list(range(_from, to + 1))
+    fig = figure(title="AAPL data", x_axis_type = "datetime")
+    fig.line(aapl['Date'], aapl['Close'], color=colors[color], line_width=2)
+
+    resources = INLINE.render()
+
+    script, div = components(fig)
+    html = flask.render_template(
+        'embed.html',
+        plot_script=script,
+        plot_div=div,
+        resources=resources,
+        color=color
+        #_from=_from,
+        #to=to
+    )
+    
+    return html
+
+
 
 if __name__ == '__main__':
-  app.run(port=33507)
+    print(__doc__)
+    app.run(port=33507)
+
